55b70f95349679160a5019fa5dddb00bd62c2751
Reeann <reeannvirginia@gmail.com>
2020-05-07 15:14:24 -0400

    ignore files

--- dist/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NO_USER = 'User does not exist in our system';
-exports.INVALID_PASSWORD = 'Invalid password';
-exports.LOGGED_IN = 'Successfully logged in!';
-exports.SIGNED_UP = 'Registration completed';
-//# sourceMappingURL=constants.js.map
